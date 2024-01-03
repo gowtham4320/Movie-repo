69a9789246ae2713d5b32af42686a885af7ed090
gowtham4320 <gowthamsivakumar4320@gmail.com>
2024-01-03 20:13:15 +0530

    added favicon in build

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const CopyPlugin = require('copy-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.tsx',
@@ -45,10 +45,13 @@ module.exports = {
             template: path.resolve(__dirname, "public", "index.html"),
             filename: "index.html",
         }),
+        new CopyPlugin({
+            patterns: [{ from: 'public/favicon.ico' }],
+        }),
     ],
     output: {
         filename: 'bundle.js',
-        path: path.resolve("https://movie-repo-seven.vercel.app/", 'build'),
+        path: path.resolve(__dirname, 'build'),
         clean: true,
     },
 };
